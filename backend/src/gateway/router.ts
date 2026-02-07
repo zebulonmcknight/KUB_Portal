1b9705b8a98ff0015c5ba09143bfcacac2872ea0
zebulonmcknight <zmcknigh@vols.utk.edu>
2026-02-07 13:07:07 -0500

    beginning controller files/starter test api endpoints

--- backend/src/gateway/router.ts
+++ backend/src/gateway/router.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-const loginController = require('./controllers/auth.controller');
+const authController = require('./controllers/auth.controller');
+const billingController = require('./controllers/billing.controller');
+const accountController = require('./controllers/account.controller'); 
 
 const router = Router(); 
 
-router.post('/auth/login', loginController.login); 
+router.post('/auth/login', authController.login);
+router.post('/billing/payment', billingController.payment);
+router.post('/account/profile', accountController.profile); 
 
 export default router; 
