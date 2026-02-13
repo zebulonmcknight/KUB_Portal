25a76047bcf95dde0b3f60a871a5ac4fdc8af757
cramosme <chrisramoscr.cr@gmail.com>
2026-02-13 14:56:31 -0500

    Add tabs layout and profile/outages screens
    Introduce tab-based layout and basic screens: add app/(tabs)/_layout.tsx to set up Tabs with Billing and Profile routes and added simple Outages and Profile screens. Extend tailwind.config.js theme with app color tokens for primary, section, icons, text, outages, and chatbot styling.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,5 +2,12 @@ import { Stack } from "expo-router";
 import './globals.css';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="(tabs)"
+        options={{headerShown: false}}
+      />
+    </Stack>
+  );
 }
