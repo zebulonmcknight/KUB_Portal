e86eb4cb35bef4acb942fd2a2df8716a6a7ddc14
cramosme <chrisramoscr.cr@gmail.com>
2026-02-17 10:36:29 -0500

    Got Tab bar navigation setup

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,37 @@
+import { highlightedIcons, icons } from '@/constants/icons';
 import { Tabs } from 'expo-router';
 import React from 'react';
+import { Image, View } from 'react-native';
+
+const TabIcon = ({focused, icon, title}: any) => {
+
+   if( focused ){
+      return(
+         <View>
+            <Image source={icon} style={{ width: 24, height: 24 }}/>
+         </View>
+      )
+   }
+   return(
+      <View>
+         <Image source={icon} style={{ width: 24, height: 24 }}/>
+      </View>
+   )
+}
 
 const _layout = () => {
    return (
       <Tabs
          screenOptions={{
-            tabBarShowLabel: false,
-            tabBarItemStyle: {
+            tabBarShowLabel: true,
+            tabBarStyle: {
+               backgroundColor: '#091C3C',
+               height: 106,
                width: '100%',
-               height: '100%',
-               justifyContent: 'center',
-               alignItems: 'center',
+               position: 'absolute',
+               overflow: 'hidden',
+               borderWidth: 1,
+               borderColor: "#091C3C"
             }
          }}
       >
@@ -19,6 +40,13 @@ const _layout = () => {
             options={{
                title: 'Billing',
                headerShown: true,
+               tabBarIcon: ({ focused }) => (
+                  <TabIcon
+                     focused={focused}
+                     icon={!focused ? icons.billing : highlightedIcons.highlightedBilling}
+                     title='Billing'
+                  />
+               )
             }}
          />
          <Tabs.Screen
@@ -26,10 +54,45 @@ const _layout = () => {
             options={{
                title: 'Profile',
                headerShown: true,
+               tabBarIcon: ({ focused }) => (
+                  <TabIcon
+                     focused={focused}
+                     icon={!focused ? icons.profile : highlightedIcons.highlightedProfile}
+                     title='Profile'
+                  />
+               )
+            }}
+         />
+         <Tabs.Screen
+            name='outages'
+            options={{
+               title: 'Outages',
+               headerShown: true,
+               tabBarIcon: ({ focused }) => (
+                  <TabIcon
+                     focused={focused}
+                     icon={!focused ? icons.outages : highlightedIcons.highlightedOutages}
+                     title='Outages'
+                  />
+               )
+            }}
+         />
+         <Tabs.Screen
+            name='chatbot'
+            options={{
+               title: 'Q&A',
+               headerShown: true,
+               tabBarIcon: ({ focused }) => (
+                  <TabIcon
+                     focused={focused}
+                     icon={!focused ? icons.chatbot : highlightedIcons.highlightedChatbot}
+                     title='Q&A'
+                  />
+               )
             }}
          />
       </Tabs>
-   )
+   );
 }
 
 export default _layout;
