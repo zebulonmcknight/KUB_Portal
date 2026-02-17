2f09db002e6cbd773a04419660a2c73168d02603
cramosme <chrisramoscr.cr@gmail.com>
2026-02-17 12:34:45 -0500

    Refactor tab layout and add global theme
    Simplify tab UI and apply a global theme to the application.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,30 @@
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import './globals.css';
 
+const globalTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#091C3C',
+    text: '#F7FDFD',
+  },
+};
+
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="(tabs)"
-        options={{headerShown: false}}
-      />
-    </Stack>
+    <ThemeProvider value={globalTheme}>
+      <Stack screenOptions={{
+        contentStyle:{backgroundColor: '#091C3C'}
+      }}>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+          }}
+          
+        />
+      </Stack>
+    </ThemeProvider>
   );
 }
