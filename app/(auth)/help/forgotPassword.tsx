b885c2e2f27e7b54f503ef0f315186d29818e02d
cramosme <chrisramoscr.cr@gmail.com>
2026-03-03 09:49:37 -0500

    Start working on forgot username page

--- app/(auth)/help/forgotPassword.tsx
+++ app/(auth)/help/forgotPassword.tsx
@@ -1,14 +1,18 @@
+import { Stack } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function ForgotPassword() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View className="flex-1 justify-center items-center">
+      <Stack.Screen 
+         options={{
+            title: "Forgot Password", // Set the header title for this screen
+            headerStyle: {
+               backgroundColor: '#3377F4', // Match the header background to active theme
+            },
+            headerShadowVisible: false, // Remove the shadow underneath header for seamless integration with background
+         }}
+      />
       <Text className="text-text_main">Edit (auth)/help/forgotPassword.tsx to edit this screen.</Text>
     </View>
   );
--- app/(auth)/help/forgotUsername.tsx
+++ app/(auth)/help/forgotUsername.tsx
@@ -1,15 +1,30 @@
+import { Stack } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function ForgotUsername() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-text_main">Edit (auth)/help/forgotUsername.tsx to edit this screen.</Text>
+    <View className="flex-1 justify-center">
+      <Stack.Screen 
+         options={{
+            title: "Forgot Username", // Set the header title for this screen
+            headerStyle: {
+               backgroundColor: '#3377F4', // Match the header background to active theme
+            },
+            headerShadowVisible: false, // Remove the shadow underneath header for seamless integration with background
+         }}
+      />
+      <View className="flex-1 justify-start left-4 pt-4">
+        <Text className="text-text_main font-sans text-2xl tracking-wide p-4">
+          Please enter your information:
+        </Text>
+        <View>
+          
+        </View>
+
+        {/* Text box with 'Email Address' prefilled inactive color. When clicked the email address text becomes active and becomes part of the box */}
+        {/* Under text box is inactive next button. Button becomes active when user types *@*.com [asterisk can be anything, works with just one letter] */}
+
+      </View>
     </View>
   );
 }
