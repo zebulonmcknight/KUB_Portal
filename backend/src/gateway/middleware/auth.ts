1372410e5d2211d409fc34a3f879b5bac0f573bb
zebulonmcknight <zmcknigh@vols.utk.edu>
2026-03-04 13:23:15 -0500

    minor middleware updates to match new name convention

--- backend/src/gateway/middleware/auth.ts
+++ backend/src/gateway/middleware/auth.ts
@@ -3,7 +3,7 @@ import { auth } from 'express-oauth2-jwt-bearer';
 
 export const checkJwt = auth({
     audience: process.env.AUTH0_AUDIENCE, 
-    issuerBaseURL: 'https://${process.env.AUTH0_DOMAIN}', 
+    issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`, 
     tokenSigningAlg: 'RS256'
 }); 
 
