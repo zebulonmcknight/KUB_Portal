73e9a37fec58c070a253f122d258e812db92c997
Kevin Lam <kevlam04@gmail.com>
2026-04-18 17:51:48 -0400

    Updated similarity_threshold and updated test.py
    Lowered similarity_threshold to 0.3 to find more matches. Also added a loop to test multiple questions per run.

--- customer_service/rag.py
+++ customer_service/rag.py
@@ -32,14 +32,14 @@ Do not use any markdown formatting. Write in plain text only. For links, write t
 class ChatRequest(BaseModel):
     question: str
 
-def search_faq(query: str, similarity_threshold: float = 0.5, num_match: int = 1):
+def search_faq(query: str, similarity_threshold: float = 0.3, num_match: int = 1):
     """
     Uses rpc() to call match_faq query function to perform semantic vector search over the faq table's embedding 
     column using negative inner product to find AT MOST num_match entries that have a similarity >= similarity_threshold.
 
     Args:
         query: User's question text.
-        similarity_threshold: Mininmum similarity score (default 0.5).
+        similarity_threshold: Mininmum similarity score (default 0.3).
         num_match: Maximum number of matches to return (default 1).
         
     Returns:
